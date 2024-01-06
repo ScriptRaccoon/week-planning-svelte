29c20b873ff61ed4a1b0b918a164a4a5d9c01094
Script Raccoon <scriptraccoon@gmail.com>
2024-01-06 12:10:11 +0100

    sync with localstorage

--- src/stores.ts
+++ src/stores.ts
@@ -1,5 +1,30 @@
-import { writable } from "svelte/store"
+import { writable, type Writable } from "svelte/store"
 import type { PlanData } from "./types"
 
+function local_store<T>(
+	key: string,
+	default_value: T,
+	update: (value: T) => void = () => {}
+): Writable<T> {
+	let initial_value: T
+	try {
+		const has_stored_value = localStorage.getItem(key) !== null
+		initial_value = has_stored_value
+			? JSON.parse(localStorage.getItem(key)!)
+			: default_value
+	} catch (_) {
+		initial_value = default_value
+	}
+
+	const store = writable<T>(initial_value)
+
+	store.subscribe((value: T) => {
+		localStorage.setItem(key, JSON.stringify(value))
+		update(value)
+	})
+
+	return store
+}
+
 export const editing_id = writable<string | null>(null)
-export const plans = writable<Record<string, PlanData[]>>({})
+export const plans = local_store<Record<string, PlanData[]>>("plans", {})
