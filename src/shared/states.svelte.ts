45db06d7e2fe328140ffd568824c6c91d47a82b5
Script Raccoon <scriptraccoon@gmail.com>
2025-03-13 19:24:14 +0100

    add JSDocs for all utils

--- src/shared/states.svelte.ts
+++ src/shared/states.svelte.ts
@@ -1,9 +1,16 @@
+/**
+ * The ID of the plan that is currently being edited.
+ * Needs to be wrapped with .value for reactivity.
+ */
 export const editingID = $state<{ value: string | null }>({ value: null })
 
 export function cancelEditing() {
 	editingID.value = null
 }
 
+/**
+ * Creates a state that is synced with the local storage.
+ */
 export function createLocalStore<T>(key: string, defaultValue: T) {
 	let initialValue = defaultValue
 
--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,31 +1,49 @@
+/**
+ * Returns a string key for a date in the format 'YYYY-MM-DD'.
+ */
+export function key(date: Date): string {
+	return date.toISOString().slice(0, 10)
+}
+
+/**
+ * Returns the first day of the week of the given date.
+ */
 export function getWeekStart(date: Date) {
 	const weekStart = new Date(date)
 	weekStart.setDate(date.getDate() - dayIndex(date))
 	return weekStart
 }
 
+/**
+ * Returns the last day of the week of the given date.
+ */
 export function getWeekEnd(date: Date) {
 	const weekEnd = new Date(date)
 	weekEnd.setDate(date.getDate() + (6 - dayIndex(date)))
 	return weekEnd
 }
 
+/**
+ * Monday = 0, Tuesday = 1, ..., Sunday = 6
+ */
+function dayIndex(date: Date) {
+	return (date.getDay() + 6) % 7
+}
+
+/**
+ * Returns a new date that is one week after the given date.
+ */
 export function addOneWeek(date: Date) {
 	const newDate = new Date(date)
 	newDate.setDate(date.getDate() + 7)
 	return newDate
 }
 
+/**
+ * Returns a new date that is one week before the given date.
+ */
 export function subtractOneWeek(date: Date) {
 	const newDate = new Date(date)
 	newDate.setDate(date.getDate() - 7)
 	return newDate
-}
-
-function dayIndex(date: Date) {
-	return (date.getDay() + 6) % 7
-}
-
-export function key(date: Date): string {
-	return date.toISOString().slice(0, 10)
 }
